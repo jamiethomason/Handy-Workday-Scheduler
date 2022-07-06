f1fd91edb8c9d0bdf3209522d0c2160ee6307d9a
Your Name <JamieLou@jamieresmacbook.lan>
2022-07-06 16:32:19 -0400

    pseudo-coding task

---
+++ Develop/script.js
@@ -0,0 +1,9 @@
+var currentDate = document.getElementbyId('#currentDay');
+var timeBlocks = document.getElementbyId('#timeblocks');
+
+
+//Show Date in Heading- Weekday, Month, Day
+
+//Timeblock Row Grids- 9 a.m. to 5 p.m., will color code through CSS for past/present/future
+
+//local storage for Timeblock events
