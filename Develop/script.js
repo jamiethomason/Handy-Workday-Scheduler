e1e0c337f200f0d5e0967ec2b43e2181ed697dfa
Your Name <JamieLou@jamieresmacbook.lan>
2022-07-07 22:16:49 -0400

    established rows for tasks

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,44 @@
-var currentDate = document.getElementbyId('#currentDay');
-var timeBlocks = document.getElementbyId('#timeblocks');
+var currentDate = document.querySelector('#currentDay');
+var tasks = {};
+var saveBtn = document.querySelector('.saveBtn');
+var textAreas = $('.time-block');
+var currentHour = moment().hour(); // Number
 
 
-//Show Date in Heading- Weekday, Month, Day
 
-//Timeblock Row Grids- 9 a.m. to 5 p.m., will color code through CSS for past/present/future
+
+//Show Date in Heading
+var currentDay = function() {
+currentDate.textContent = moment().format("MMM Do YY");
+}
+
+//Show if Task is .past/.present/.future
+
+for (var index = 0; index < textAreas.length; index++) {
+    var element = textAreas[index];
+    // element.style.backgroundColor = "red";
+    var rowSlotTime = $(element).siblings(".hour").attr('id');
+
+//If less than, equal to, or more than it will change color (if, else if, else)
+
+}
+
+//saveBtn at end of each row
+
+
+//Delete Tasks
 
 //local storage for Timeblock events
+
+ $('.saveBtn').on('click', function() {
+    var value = $(this).siblings('.description').val();
+    var time = $(this).parent().attr('id');
+
+     console.log(value,time);
+ })
+
+var timeblockTasks = function() {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+}
+
+currentDay()
