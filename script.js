fd253630570f31e268d537d1b9212022bdf3790e
Jamie Thomason <jamie.revill@gmail.com>
2022-07-11 22:03:06 -0400

    adding moment.js to timeslots

--- script.js
+++ script.js
@@ -4,9 +4,6 @@ var saveBtn = document.querySelector('.saveBtn');
 var textAreas = $('.time-block');
 var currentHour = moment().hour(); // Number
 
-
-
-
 //Show Date in Heading
 var currentDay = function() {
 currentDate.textContent = moment().format("MMM Do YY");
@@ -16,11 +13,18 @@ currentDate.textContent = moment().format("MMM Do YY");
 
 for (var index = 0; index < textAreas.length; index++) {
     var element = textAreas[index];
+    // console.log(element);
     // element.style.backgroundColor = "red";
+    // console.log($(element).siblings());
     var rowSlotTime = $(element).siblings(".hour").attr('id');
+    console.log(rowSlotTime);
+    var timeblockHour = moment(rowSlotTime, "HH:mm");
+    console.log(timeblockHour);
 
-//If less than, equal to, or more than it will change color (if, else if, else)
-
+//If less than, equal to, or more than currentHour, it will change color (if, else if, else)
+    if (rowSlotTime) {
+        rowSlotTime < currentHour = element.style.backgroundColor = ".past";
+    }
 }
 
 //Delete Tasks
